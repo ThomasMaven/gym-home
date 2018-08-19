2a3ae4754584f4499eee1c3718530844885b2540
Jakub Piecuch <jakub.piecuch@jakubpiecuch.pl>
2018-08-19 21:07:40 +0200

    Update security

---
+++ docker.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+docker build ./accounts/ -t accounts
+docker build ./authorization/ -t authorization
+docker build ./config/ -t config
+docker build ./eureka/ -t eureka
+docker build ./web/ -t web
